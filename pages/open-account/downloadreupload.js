9981dbaa6bfed27053af9fa6a0d8ce4bc0d2c55d
Basitec <144648167+Basitec@users.noreply.github.com>
2025-02-15 20:34:24 +0100

    Created downloadreupload.js

---
+++ pages/open-account/downloadreupload.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const downloadreupload = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima praesentium repellat provident. Tempora fugit, pariatur deserunt quas eum autem tempore perferendis qui dignissimos ducimus rem voluptatem incidunt, atque ex dicta.
+    </div>
+  )
+}
+
+export default downloadreupload

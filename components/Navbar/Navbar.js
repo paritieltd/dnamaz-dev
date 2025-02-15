1a9af7ca4747e90f866c01d1e3023396cc8336bc
Basitec <144648167+Basitec@users.noreply.github.com>
2025-02-15 15:44:18 +0100

    Update Navbar.js

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { FaTimes, FaBars, FaChevronDown } from "react-icons/fa";
 import Dropdown from "../Dropdown/Dropdown";
-import { AboutMenuItems, FocusMenuItems } from "../Dropdown/DropdownItems";
+import { AboutMenuItems, FocusMenuItems,ProductMenuItems } from "../Dropdown/DropdownItems";
 import styles from "./Navbar.module.css";
 import Image from "next/image";
 import logo from "../../public/images/Dnamaz-logoo.png";
@@ -11,10 +11,11 @@ function Navbar() {
   const [click, setClick] = useState(false);
   const [aboutDropdown, setAboutDropdown] = useState(false);
   const [focusDropdown, setFocusDropdown] = useState(false);
+  const [productDropdown, setProductDropdown] = useState(false);
   const [navBg, setNavBg] = useState(false);
   const [rotateCheveronAbout, setRotateCheveronAbout] = useState(false);
   const [rotateCheveronFocus, setRotateCheveronFocus] = useState(false);
-
+  const [rotateCheveronProduct, setRotateCheveronProduct] = useState(false);
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
@@ -26,6 +27,10 @@ function Navbar() {
     setFocusDropdown(true);
     setRotateCheveronFocus(true);
   };
+  const onMouseEnterProduct=()=>{
+    setProductDropdown(true);
+    setRotateCheveronProduct(true);
+  }
 
   const onMouseLeaveAbout = () => {
     setAboutDropdown(false);
@@ -35,6 +40,10 @@ function Navbar() {
     setFocusDropdown(false);
     setRotateCheveronFocus(false);
   };
+  const onMouseLeaveProduct=()=>{
+    setProductDropdown(false);
+    setRotateCheveronProduct(false);
+  }
 
   const changeBg = () => {
     // (window.scrollY);
@@ -169,8 +178,8 @@ function Navbar() {
                 />
               )}
             </li>
-            <li className={styles.nav_item}>
-              <a
+            {/* <li className={styles.nav_item}>
+              {/* <a
                 className={`${
                   router.pathname === "/blog"
                     ? "text-[#1D5506] !font-extrabold"
@@ -180,10 +189,32 @@ function Navbar() {
                   router.push("/blog");
                   handleClick;
                 }}
-              >
-                Blog
-              </a>
-            </li>
+              > */}
+             <li onMouseEnter={onMouseEnterProduct} onMouseLeave={onMouseLeaveProduct} className={styles.nav_item}>
+                <div className={`!flex lg:block justify-center ${styles.nav_links}`}>
+                  <div
+            className={`flex items-center space-x-1 ${
+              router.pathname === "/Dnamaz-capital"
+              ? "text-[#1D5506] !font-extrabold"
+              : router.pathname === "/open-account"
+              ? "text-[#1D5506] !font-extrabold"
+              :""
+            }`}
+            >
+            <span className="font-bold hover:text-primary cursor-pointer">
+            Our Product
+              </span>
+              <span className={`${rotateCheveronProduct ? "-rotate-90" : ""}`}>
+                <FaChevronDown size={15} />
+              </span>
+            </div>
+          </div>
+          {/* <Link href="/blog"></Link> */}
+          {productDropdown && <Dropdown menuItems={ProductMenuItems} />}
+        </li>
+                {/* Our Product */}
+              {/* </a> 
+            </li> */}
             <li className={`${styles.nav_item}`}>
               <a
                 className="text-center inline-flex justify-center items-center h-[40px] w-[180px] bg-custom-primary cursor-pointer text-white rounded-xl mx-auto"

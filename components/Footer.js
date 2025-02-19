5e51c47a699f58be00c61907f9c6854019238048
Basitec <144648167+Basitec@users.noreply.github.com>
2025-02-19 21:54:38 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -55,7 +55,7 @@ const Footer = () => {
               Central Business District, Fct-Abuja.
             </p>
             <span className="block my-2"><a href="mailto:enquiries@dnamazcapital.com">enquiries@dnamazcapital.com</a></span>
-            <span className="block mb-2"><a href="tel:+234 916 444 1444">+234 916 444 1444</a></span>
+            <span className="block mb-2"><a href="tel:+234 9164441444">+234 916 444 1444</a></span>
           </div>
           <div className="">
             <h2 className="text-xl font-bold mb-6 font-lex">Other Links</h2>

5193c4d2ea65ec35e01571fc5f09260ef34844c5
Basitec <144648167+Basitec@users.noreply.github.com>
2025-02-19 05:11:36 +0100

    Update index.js

--- pages/open-account/index.js
+++ pages/open-account/index.js
@@ -47,7 +47,7 @@ const OpenAccount = () => {
           </div>
         </div>
         <section id="dnamazCapital" className=" w-[70%] text-center mx-auto lg:py-[10rem] ">
-          <h4 className="lg:text-[36px] max-sm:text-[20px] text-center my-5 mb-10 font-bold text-custom-primary">D'Namaz Capital Halal Fixed Income Fund</h4>
+          <h4 className="lg:text-[36px] max-sm:text-[20px] text-center my-5 mb-10 font-bold text-custom-primary">D'Namaz Halal Fixed Income Fund</h4>
           <div className=" flex max-sm:flex-col max-sm:gap-4 lg:w-[90%] lg:mx-auto items-center justify-between">
           <button
           className="hover:bg-custom-primary hover:text-white transition-all duration-500 text-custom-primary font-semibold border border-custom-primary h-16 w-full sm:w-[unset] sm:px-20 "
